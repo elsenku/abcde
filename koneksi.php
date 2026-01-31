0450fc4a54c0c95a496c38e8891fc4cadbfb0586
elsenku <finsuwito@gmail.com>
2026-01-31 15:35:16 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -7,11 +7,7 @@ $db   = "db_peminjaman";
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if ($koneksi) {
-    // echo '<b>nama db : </b>'.$db;
-    // echo '<br>';
-    // echo("Hore konek");
 } else {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
-
